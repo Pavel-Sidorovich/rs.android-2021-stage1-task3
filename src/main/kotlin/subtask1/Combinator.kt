3fd92893b2280d41e3eac1dc950bab9e350fa122
Pavel Sidorovich <sidorovichpavelalex@gmail.com>
2021-07-13 22:55:42 +0300

    feat: implement task 3

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -3,7 +3,27 @@ package subtask1
 class Combinator {
 
     // TODO: Complete the following function
+    var map = HashMap<Int, Long>()
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val n = array[1]
+        val c = array[0].toLong()
+        var i = 1
+        var temp = 1L
+        var z = 1
+
+        while (i <= n) {
+            temp *= i
+            map[i] = temp
+            i ++
+        }
+
+        while (z != n) {
+            val k = map[n]!! / (map[n - z]!! * map[z]!!)
+            if (k == c) {
+                return z
+            }
+            z ++
+        }
+        return null
     }
 }
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,48 @@
 package subtask2
 
+import java.util.ArrayList
+import kotlin.math.sqrt
+
 class SquareDecomposer {
+    val map = HashMap<Int, Long>()
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        if (number < 0) {
+            return null
+        }
+        var i = 0
+        do {
+            val square = i * i.toLong()
+            map[i] = square
+            i++
+        } while (i <= sqrt(number.toDouble()))
+
+        return decomposer(number, number * number.toLong())?.dropLast(1)?.toTypedArray()
+    }
+
+    private fun decomposer(n: Int, remain: Long): MutableList<Int>? {
+        // basic case
+        if (remain == 0L) {
+            val r: MutableList<Int> = ArrayList()
+            r.add(n)
+            return r
+        }
+
+        // iterate all element less than n
+        for (i in n - 1 downTo 1) {
+            if (remain - (map[i] ?: i * i.toLong()) >= 0) {
+                val r = decomposer(i, remain - (map[i] ?: i * i.toLong()))
+
+                // only get the answer
+                if (r != null) {
+                    r.add(n)
+                    return r
+                }
+            }
+        }
+
+        // no answer
+        return null
     }
 }
